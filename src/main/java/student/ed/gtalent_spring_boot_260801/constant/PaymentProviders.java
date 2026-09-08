54f52a2eb16c0047769ae5e9e08964efa4f6b7ca
EdwardChung <leonardo071123@gmail.com>
2026-09-08 21:41:27 +0800

    feat: newebpay part I

---
+++ src/main/java/student/ed/gtalent_spring_boot_260801/constant/PaymentProviders.java
@@ -0,0 +1,10 @@
+package student.ed.gtalent_spring_boot_260801.constant;
+
+public final class PaymentProviders {
+
+    // 藍新金流
+    public static final String NEWEBPAY = "NEWEBPAY";
+
+    private PaymentProviders() {
+    }
+}
