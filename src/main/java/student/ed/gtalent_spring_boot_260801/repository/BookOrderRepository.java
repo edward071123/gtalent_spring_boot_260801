d0a09fd1432de803179b6de70390e2c92b3b1276
EdwardChung <leonardo071123@gmail.com>
2026-09-17 18:39:46 +0800

    fix: newebpay partIV

--- src/main/java/student/ed/gtalent_spring_boot_260801/repository/BookOrderRepository.java
+++ src/main/java/student/ed/gtalent_spring_boot_260801/repository/BookOrderRepository.java
@@ -2,20 +2,24 @@ package student.ed.gtalent_spring_boot_260801.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import student.ed.gtalent_spring_boot_260801.entity.BookOrder;
 
 public interface BookOrderRepository extends JpaRepository<BookOrder, Long> {
-    
-    @Query(
-        value = "SELECT COUNT(*) > 0 FROM book_orders WHERE book_id = :bookId AND order_status = :orderStatus",
-        nativeQuery = true
-    )
-    public boolean existsByBookIdAndOrderStatus(Long bookId, String orderStatus);
 
     @Query(
-        value = "SELECT COUNT(*) > 0 FROM book_orders WHERE order_no = :orderNo",
+        value = "SELECT COUNT(*) FROM book_orders WHERE book_id = :bookId AND order_status = :orderStatus",
         nativeQuery = true
     )
-    public boolean existsByOrderNo(String orderNo);
+    long countByBookIdAndOrderStatus(
+        @Param("bookId") Long bookId,
+        @Param("orderStatus") String orderStatus
+    );
+
+    @Query(
+        value = "SELECT COUNT(*) FROM book_orders WHERE order_no = :orderNo",
+        nativeQuery = true
+    )
+    long countByOrderNo(@Param("orderNo") String orderNo);
 }
